3834890ed6221c5752aef75bac27fcf839a87723
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-29 11:06:36 +0100

    feat: add auth routes to server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+// API Endpoints
 app.get('/', (req, res) => res.json('API is working'));
+app.use('/auth', authRouter);
 
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
