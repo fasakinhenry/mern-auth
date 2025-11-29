4baf825cd565bb4e019702ca8b69d110e667c5c5
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-29 10:19:03 +0100

    chore: update .env.example with JWT_SECRET

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import userModel from "../models/userModel.js";
 
 export const register = async (req, res) => {
@@ -9,7 +10,16 @@ export const register = async (req, res) => {
     }
 
     try {
-        
+        const existingUser = await userModel.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ success: false, message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new userModel({ name, email, password: hashedPassword });
+        await user.save();
+        res.status(201).json({ success: true, message: "User registered successfully" });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
