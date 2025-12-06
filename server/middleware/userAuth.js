69effa935e6e82170c7186e1a7068ea351949d0c
Fasakin Henry <fasakinhenry@gmail.com>
2025-12-06 07:24:32 +0100

    feat: create the user authentication middleware

---
+++ server/middleware/userAuth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+const userAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.json({
+      success: false,
+      message: 'Not authorized. Login Again!',
+    });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (tokenDecode.id) {
+      req.body.userId = tokenDecode.id;
+    } else {
+      return res.json({
+        success: false,
+        message: 'Not authorized. Login Again!',
+      });
+    }
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+  next();
+};
+
+export default userAuth;
