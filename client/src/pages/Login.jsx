394b803d12074899769f09fc7fa19086e4e3a8e1
Fasakin Henry <fasakinhenry@gmail.com>
2025-12-07 20:28:17 +0100

    feat: add simple submit handler to login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { assets } from '../assets/assets';
 import { useNavigate } from 'react-router-dom';
+import { AppContent } from '../context/AppContext';
+import axios from 'axios';
 
 const Login = () => {
   const navigate = useNavigate();
+
+  const { backendUrl, setIsLoggedin } = useContext(AppContent);
+
   const [state, setState] = useState('Sign Up');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      axios.defaults.withCredentials = true;
+      if (state === 'Sign Up') {
+        const { data } = await axios.post(backendUrl + '/api/auth/register', {
+          name,
+          email,
+          password,
+        });
+        if (data.success) {
+          setIsLoggedin(true);
+          navigate('/');
+        } else {
+          alert(data.message);
+        }
+      } else {
+      }
+    } catch (error) {}
+  };
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400'>
       <img
@@ -26,7 +52,7 @@ const Login = () => {
             : 'Please enter your credentials to login.'}
         </p>
 
-        <form>
+        <form onSubmit={onSubmitHandler}>
           {state === 'Sign Up' && (
             <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
               <img src={assets.person_icon} alt='Person Icon' />
