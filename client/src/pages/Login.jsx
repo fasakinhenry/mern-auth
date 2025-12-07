9889cbef6fe8d1aa24b2d9e9486a2d9e5a503443
Fasakin Henry <fasakinhenry@gmail.com>
2025-12-07 20:02:31 +0100

    feat: add state to the login inputs

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,100 @@
+import { useState } from 'react';
+import { assets } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
+  const navigate = useNavigate();
+  const [state, setState] = useState('Sign Up');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400'>
+      <img
+        onClick={() => navigate('/')}
+        src={assets.logo}
+        alt='Logo'
+        className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'
+      />
+      <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full max-w-md sm:w-96 text-indigo-300 text-sm'>
+        <h2 className='text-3xl font-semibold mb-3 text-white text-center'>
+          {state === 'Sign Up' ? 'Create Account' : 'Login Account'}
+        </h2>
+        <p className='text-sm mb-6 text-center'>
+          {state === 'Sign Up'
+            ? 'Please fill in the form to create an account.'
+            : 'Please enter your credentials to login.'}
+        </p>
+
+        <form>
+          {state === 'Sign Up' && (
+            <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+              <img src={assets.person_icon} alt='Person Icon' />
+              <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                className='bg-transparent border-none outline-none text-white placeholder-gray-400'
+                type='text'
+                placeholder='Full Name'
+                required
+              />
+            </div>
+          )}
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+            <img src={assets.mail_icon} alt='Mail Icon' />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className='bg-transparent border-none outline-none text-white placeholder-gray-400'
+              type='email'
+              placeholder='Email'
+              required
+            />
+          </div>
+
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+            <img src={assets.lock_icon} alt='Lock Icon' />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              className='bg-transparent border-none outline-none text-white placeholder-gray-400'
+              type='password'
+              placeholder='Password'
+              required
+            />
+          </div>
+          <p
+            onClick={() => navigate('/reset-password')}
+            className='mb-4 text-indigo-500 cursor-pointer'
+          >
+            Forgot Password?
+          </p>
+          <button className='w-full py-2.5 rounded-full bg-linear-to-r from-indigo-500 to-indigo-900 text-white font-medium cursor-pointer hover:opacity-90 transition-all'>
+            {state}
+          </button>
+          {state === 'Sign Up' ? (
+            <p className='text-gray-400 text-center text-xs mt-4'>
+              Already have an account?{' '}
+              <span
+                onClick={() => setState('Login')}
+                className='text-blue-400 cursor-pointer underline'
+              >
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className='text-gray-400 text-center text-xs mt-4'>
+              Don't have an account?{' '}
+              <span
+                onClick={() => setState('Sign Up')}
+                className='text-blue-400 cursor-pointer underline'
+              >
+                Sign up
+              </span>
+            </p>
+          )}
+        </form>
+      </div>
     </div>
   );
 };
